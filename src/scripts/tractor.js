782d9c6b9ed4b48f7f91850726ea1da1a431e034
Jeremy Myers <echelonical@gmail.com>
2022-07-29 14:19:09 -0500

    annoted tractor.js module for better understanding

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -1,40 +1,37 @@
 //import needed functions...
-import { addPlant } from "./field.js"
-import { createAsparagus } from "./seeds/Asparagus.js"
-import { createCorn } from "./seeds/Corn.js"
-import { createPotato } from "./seeds/potato.js"
-import { createSoybean } from "./seeds/soybean.js"
-import { createSunflower } from "./seeds/Sunflower.js"
-import { createWheat } from "./seeds/Wheat.js"
+import { addPlant } from "./field.js" //this functions pushes seedObjects to array (plantArray on field.js module)
+import { createAsparagus } from "./seeds/Asparagus.js" //creates a object/seed
+import { createCorn } from "./seeds/Corn.js"        //creates a object/seed
+import { createPotato } from "./seeds/potato.js"    //creates a object/seed
+import { createSoybean } from "./seeds/soybean.js"  //creates a object/seed
+import { createSunflower } from "./seeds/Sunflower.js"  //creates a object/seed
+import { createWheat } from "./seeds/Wheat.js"  //creates a object/seed
 import { usePlantsArray } from './field.js';  //plants array from field.js
 
-
+//invoke imported functions and store in variable.
 const asparagusSeed = createAsparagus()
 const cornSeed = createCorn()
 const potatoSeed = createPotato()
 const soybeanSeed = createSoybean()
 const sunflowerSeed = createSunflower()
 const wheatSeed = createWheat()
-// const plantArray = usePlantsArray()
 
-//declare & export function plantSeeds that will accept an input (year's planting plan).
-//iterate the row of food types to be planted
-//----->if food is asparagus invoke the createAsparagus function to get a seed)
-//take the seed and add it to the array of plants in the field module  (plantArray).
+//1. declare & export function plantSeeds that will accept an input (year's planting plan).
+//2. iterate the row of food types to be planted (iterate through both parent/main and child/nested arrays)
+//3. ----->if food is asparagus invoke the createAsparagus function to get a seed...conditional to invoke createFunction/seedObject.
+//4. take the seed and add it to the array of plants in the field module  (plantArray)...Have no idea how I figured this part out.........!!!!!!!!
 
 
-//declare and export a function plantSeeds
-//input actPlantPlan
+//declare and export a function plantSeeds input plantArray
 export const plantSeeds = (plantPlanArray) => {
 
-    //import seed functions to be invoked and store in variables to be used.
-    //iterate the array rows and invoke createSeed function whenever plant is presented.
+    //iterate the Parent array rows and invoke createSeed function whenever plant is presented.
     for (const planArray of plantPlanArray) {
+        //iterate child array...Missed this detail initially...***if having trouble make sure to go back and read directions for understanding.
         for (const plan of planArray) {
             //conditional statement to invoke proper functions within arrays...
-            //example if the index is asparagus then .push createAsparagus function?
             if (plan === "Asparagus") {
-                addPlant(asparagusSeed)  //using variable declared to invoke function on main.js to push...!
+                addPlant(asparagusSeed)  //used function, addPlant, with seedObject as parameter.  This creates a object...refer field.js module for details
             } else if (plan === "Corn") {
                 addPlant(cornSeed)
             } else if (plan === "Potato") {
@@ -48,11 +45,9 @@ export const plantSeeds = (plantPlanArray) => {
             }
         }
     }
+    //return invoked usePlantsArray?????? 
+    //Why didn't me invoking it upon importing and then returning the variable work???
     return usePlantsArray();
+    //this function exports plantArray on field.js module...
 }
 
-
-// //export years plant plan array...
-// export const yearlyArray = () => {
-    //     return actPlantPlan.map(actPlantPlan => ({ ...actPlantPlan }))
-    // }
